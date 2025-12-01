7ed0b0fe49e1b735cecacf9f66049b46deb88fe5
Joaquin Morales <joaquinmorales13a06@gmail.com>
2025-12-01 23:07:29 +1100

    Add Appwrite configuration and update dependencies in package.json

---
+++ libs/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+	endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+	platform: "com.jmc.food_ordering",
+	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
